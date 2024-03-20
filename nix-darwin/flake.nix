2e3bdac39633bb7f7ada68e48e18ea6d42d7a1ac
Baily Case <bailycase99@gmail.com>
2024-03-20 01:57:33 -0500

    fix: latest

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -5,47 +5,76 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+    let
+      configuration = { pkgs, ... }: {
+        # Setup node version 
+        system.activationScripts.fnm = {
+          text = ''
+            # Check if the default Node version is installed
+            if [ -z "$(fnm list | grep '^v19')" ]; then
+              # Install and use the default Node version
+              fnm install 19
+              fnm default 19
+            fi
+          '';
+          deps = [ ];
+        };
+
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+        # nix.package = pkgs.nix;
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true; # default shell on catalina
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 4;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        # Allow compilation of x86_64-darwin packages on aarch64-darwin.
+        nix.extraOptions = ''
+          extra-platforms = x86_64-darwin aarch64-darwin
+        '';
+      };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."Bailys-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.bailycase.imports = [
+                # NixVim module
+                #nixvim.homeManagerModules.nixvim
+                ./modules/home-manager
+              ];
+            };
+          }
         ];
+      };
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "x86_64-darwin";
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."Bailys-MacBook-Pro".pkgs;
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."Bailys-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."Bailys-MacBook-Pro".pkgs;
-  };
 }
---
+++ nix-darwin/modules/home-manager/default.nix
@@ -0,0 +1,54 @@
+{ pkgs, ... }: {
+  # specify my home-manager configs
+  imports = [
+    #./settings/zsh.nix
+    #./settings/kitty.nix
+    #./settings/tmux.nix
+#    ./settings/neovim.nix
+  ];
+
+  home = {
+    username = "bailycase";
+    homeDirectory = "/Users/bailycase";
+    packages = with pkgs; [
+          neovim
+          fzf
+          fnm
+          cargo
+          go
+          git
+          lazygit
+          kitty
+    ];
+
+    sessionVariables = {
+      PAGER = "less";
+      CLICLOLOR = 1;
+      EDITOR = "nvim";
+    };
+  };
+
+  programs = {
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    bat = {
+      enable = true;
+      config.theme = "TwoDark";
+    };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    eza.enable = true;
+    git.enable = true;
+  };
+
+  #home.file.".inputrc".source = ./settings/inputrc;
+  # Don't change this when you change package input. Leave it alone.
+  #home.stateVersion = "24.05"; # 23.11
+}
